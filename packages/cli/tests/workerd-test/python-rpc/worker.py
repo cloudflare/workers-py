c930a94c4e56528668f144e79547abfbba0fd1bf
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-05-14 12:44:34 +0900

    chore: add a comment about where the original workerd test is located

--- packages/cli/tests/workerd-test/python-rpc/worker.py
+++ packages/cli/tests/workerd-test/python-rpc/worker.py
@@ -2,6 +2,8 @@
 # Licensed under the Apache 2.0 license found in the LICENSE file or at:
 #     https://opensource.org/licenses/Apache-2.0
 
+# Ported from workerd (https://github.com/cloudflare/workerd/tree/main/src/workerd/server/tests/python/python-rpc)
+
 import collections.abc
 import json
 from asyncio import Future, sleep
