7d41ed6d6668b4aef3c5dfe0232ca07390f7fd08
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-06-18 20:00:56 +0900

    chore: lint

--- packages/cli/tests/bindings-test/src/test_analytics_engine.py
+++ packages/cli/tests/bindings-test/src/test_analytics_engine.py
@@ -9,11 +9,13 @@ async def test_is_wrapped(env):
 
 @pytest.mark.asyncio
 async def test_write_data_point_blobs_and_doubles(env):
-    env.ANALYTICS.writeDataPoint({
-        "blobs": ["blob1", "blob2"],
-        "doubles": [1.0, 2.5],
-        "indexes": ["idx"],
-    })
+    env.ANALYTICS.writeDataPoint(
+        {
+            "blobs": ["blob1", "blob2"],
+            "doubles": [1.0, 2.5],
+            "indexes": ["idx"],
+        }
+    )
 
 
 @pytest.mark.asyncio
