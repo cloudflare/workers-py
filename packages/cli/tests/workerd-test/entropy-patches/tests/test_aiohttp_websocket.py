3cd922d55b638f7448d210bdd8e317719b5a5be4
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-06-23 12:35:43 +0900

    chore: remove aiohttp for now

--- packages/cli/tests/workerd-test/entropy-patches/tests/test_aiohttp_websocket.py
+++
@@ -1,7 +0,0 @@
-# ruff: noqa: F401
-import aiohttp.http_websocket
-
-
-def test_import():
-    # make sure this file is collected by pytest
-    pass
--- packages/cli/tests/workerd-test/entropy-patches/tests/test_ssl_avoidance.py
+++ packages/cli/tests/workerd-test/entropy-patches/tests/test_ssl_avoidance.py
@@ -1,5 +1,4 @@
 # ruff: noqa: F401
-import aiohttp.connector
 import requests.adapters
 import urllib3.util.ssl_
 
