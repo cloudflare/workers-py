1c6dac97239947e41e9c80e80df87511975269a1
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-06-24 12:47:14 +0900

    chore: lint

--- packages/cli/tests/bindings-test/src/test_mtls.py
+++ packages/cli/tests/bindings-test/src/test_mtls.py
@@ -3,6 +3,7 @@ from workers._workers import _FetcherWrapper
 
 # cannot test mTLS locally so we just check the binding is properly wrapped
 
+
 @pytest.mark.asyncio
 async def test_is_fetcher_wrapper(env):
     assert isinstance(env.MY_CERT, _FetcherWrapper)
