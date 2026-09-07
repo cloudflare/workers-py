143f77fe88254b1ee2910c2df661d32c7ecabaad
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-09-07 12:38:51 +0900

    chore: remove multidict patch

--- packages/runtime-sdk/src/_workers_sdk_entropy_import_context.py
+++ packages/runtime-sdk/src/_workers_sdk_entropy_import_context.py
@@ -187,10 +187,3 @@ def openai_context(module):
         # Uses os.urandom() to generate a random signature key
         # https://github.com/openai/openai-python/blob/b19c2161b1eac80fbf1f6f67a64a50af99c53356/src/openai/lib/bedrock.py#L33
         yield
-
-
-# @register_exec_patch("multidict._multidict")
-# @contextmanager
-# def multidict_context(module):
-#     with allow_bad_entropy_calls(1):
-#         yield
